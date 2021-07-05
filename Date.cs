53fc0b2b78a1a68147188d214db2d08be47e8834
PavelKazakov1 <79844101+PavelKazakov1@users.noreply.github.com>
2021-07-05 20:48:25 +0300

    Add files via upload

---
+++ Date.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_6
+{
+    class Date
+    {
+
+        private DateTime date;
+
+
+        public Date()
+        {
+            date = Convert.ToDateTime("1.1.2009");
+            
+        }
+
+        public Date(string date)
+        {
+            this.date = DateTime.Parse(date);
+
+        }
+
+        public DateTime Date1 { get { return date; } set { date = value; } }
+        public bool LeapYear { get  { return DateTime.IsLeapYear(date.Year); } }
+
+        public DateTime PreviosDay() {
+            return date.AddDays(-1);
+        }
+
+        public DateTime NextDay()
+        {
+            return date.AddDays(+1);
+        }
+
+        public int RestDays()
+        {
+            int count = 0;
+            int month = DateTime.DaysInMonth(date.Year, date.Month);
+            for (int i = date.Day; i < month; i++) {
+                count++;
+            }
+            return count;
+        }
+
+        public DateTime this[int i]
+        {
+            get
+            {
+                 return date.AddDays(+i);
+                
+            }
+        }
+
+        static public bool operator !(Date date)
+        {
+            //date = Convert.ToDateTime(date);
+            if (date.Date1.Day == DateTime.DaysInMonth(date.Date1.Year, date.Date1.Month)) { return false; }
+            else return true;
+        }
+
+        static public bool operator true(Date date)
+        {
+            if (date.Date1.Month == 1) { return true; }
+            else return false;
+        }
+
+        static public bool operator false(Date date)
+        {
+            if (date.Date1.Month == 1) { return false; }
+            else return true;
+        }
+
+        static public bool operator &(Date date, Date date1)
+        {
+            if (date.Date1 == date1.Date1) { return true; }
+            else { return false; }
+        }
+
+        static public explicit operator string(Date date)
+        {
+            return (" Day: " + date.Date1.Day + ", Month: " + date.Date1.Month + " Year: " + date.Date1.Year);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date date1 = new Date();
+            Date date2 = new Date("15.02.2021");
+            Date date3 = new Date("01.01.2021");
+            Console.WriteLine(date2.LeapYear);
+
+            Console.WriteLine(date2.PreviosDay());
+
+            Console.WriteLine(date2.NextDay());
+
+             Console.WriteLine(date2.RestDays());
+
+            Console.WriteLine("Input index:");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(i +" days from current date is " + date1[i]);
+
+            Console.WriteLine(!date2);
+
+            if (date2) Console.WriteLine("Its begining of the year");
+            else Console.WriteLine("Its not begining of the year");
+
+            if (date2 & date3) Console.WriteLine("The same dates");
+            else Console.WriteLine("Different dates");
+            
+            Console.WriteLine((string)date1);
+            Console.ReadKey();
+
+        }
+    }
+}
